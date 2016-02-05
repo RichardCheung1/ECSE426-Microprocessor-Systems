6a78c50e099ff73d503f4025a1042b52569d18ff
RichardCheung1 <li.cheung@mail.mcgill.ca>
2016-02-05 01:58:13 -0500

    convolution and correlation not valid

--- Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -8,7 +8,111 @@ typedef struct stateInfo{
 
 extern int Kalmanfilter_asm (float* InputArray, float* OutputArray, kalman_state* kstate, int length);
 int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, int Length);
+void c_sub(float* vector_one, float* vector_two, float* output_vector, int vector_length);
+void c_mean(float* input_vector, int vector_length, float* vector_mean);
+void c_std(float* input_vector, int vector_length, float* vector_std);
+void c_correlate(float* vector_one, float* vector_two, float* vector_correlation, int vector_length);
+void c_conv(float* vector_one, float* vector_two, float* vector_convolution, int vector_length);
+int is_valid(float *ScrA, float *ScrB, int length, float tol, char *errorCode);
+int is_correct(float* actualOutput, float* controlOutput);
 
+int is_correct(float* actualOutput, float* controlOutput)
+{	
+	/*int compareLength = (sizeof(actualOutput)/sizeof(float));
+	printf("Check float length: %d\n", compareLength);*/
+
+	//Compare size of the arrays
+	if(sizeof(actualOutput) == sizeof(controlOutput)) 
+	{
+		//Compare each float in the arrays
+		for(int i = 0; i < 10; i++) 
+		{
+			printf("Check output float value: %.9g\n", actualOutput[i]);
+			printf("Check output float value: %.9g\n", controlOutput[i]);
+			//if(actualOutput[i] != controlOutput[i]) return 0;
+		}
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+void c_sub(float* vector_one, float* vector_two, float* output_vector, int vector_length)
+{
+	int i;
+	for(i = 0; i < vector_length; i++)
+	{
+		output_vector[i] = vector_one[i] - vector_two[i];
+	}
+}
+
+void c_mean(float* input_vector, int vector_length, float* vector_mean)
+{
+	int i;
+	float sum = 0;
+	for(i = 0; i < vector_length; i++) 
+	{
+		sum = sum + input_vector[i];
+	}
+
+	*vector_mean = sum / vector_length;
+}
+
+void c_std(float* input_vector, int vector_length, float* vector_std)
+{	
+	float mean, sumOfSquares =0.0;
+	c_mean (input_vector,vector_length, &mean);
+	int i;
+	for(i = 0; i < vector_length; i++) 
+	{
+		sumOfSquares += ((input_vector[i]-mean)*(input_vector[i]-mean));
+	}
+	*vector_std = sqrt(sumOfSquares/vector_length);
+}
+
+void c_correlate(float* vector_one, float* vector_two, float* vector_correlation, int vector_length)
+{
+	int correlation_length = vector_length + vector_length - 1;
+	int i;
+	for(i = 0; i < correlation_length; i++) 
+	{
+		int j = 0;
+		for(j = 0; j < vector_length; j++) 
+		{
+			if((i - j) < 0 || (i - j) >= vector_length) 
+			{
+				vector_correlation[i] = 0;
+			} 
+			else 
+			{
+				vector_correlation[i] = vector_one[j] * vector_two [j - i];
+			}
+		}
+	}
+}
+
+void c_conv(float* vector_one, float* vector_two, float* vector_convolution, int vector_length)
+{	
+	int convolution_length = vector_length + vector_length - 1;
+	int i;
+	for(i = 0; i < convolution_length; i++) 
+	{
+		int j = 0;
+		for(j = 0; j < vector_length; j++) 
+		{
+			if((i - j) < 0 || (i - j) >= vector_length) 
+			{
+				vector_convolution[i] = 0;
+			} 
+			else 
+			{
+				vector_convolution[i] = vector_one[j] * vector_two [i - j];
+			}
+		}
+	}
+}
 
 int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, int Length)
 {	
@@ -29,14 +133,15 @@ int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, 
 	return 0; 
 }
 
-int isValid(float *ScrA, float *ScrB, int length, float tol, char *errorCode)
+//checks if Source A and Source B have the same value
+int is_valid(float *ScrA, float *ScrB, int length, float tol, char *errorCode)
 {
 	
 	for(int i = 0; i < length; i++)
 	{
 		if(ScrA[i] - ScrB[i] > tol || ScrA[i] - ScrB[i] < -tol)
 		{
-			printf("Tests fails at %s\n", errorCode);
+			printf("Tests fails at %s with error tolerance %f:<SrcA:%f, SrcB:%f>\n", errorCode, tol,ScrA[i], ScrB[i]);
 			return 0;
 		}
 	}
@@ -47,18 +152,18 @@ int isValid(float *ScrA, float *ScrB, int length, float tol, char *errorCode)
 int main()
 {
 	//initialize testing array
-	float test_vector[] = {0.1f,0.2f,0.3f,0.4f,0.5f};
+	float testVector[] = {0.1f,0.2f,0.3f,0.4f,0.5f};
 	//get the size of the array
-	int length = sizeof(test_vector)/sizeof(float);
+	int length = sizeof(testVector)/sizeof(float);
 	//initiate empty output array of size length
-	float output_array_C[length];
+	float outputArrayC[length];
 	//initialize the struct at p=r=q 0.1 and x=k=0 
 	kalman_state currentState = {0.1f, 0.1f, 0.0f , 0.1f, 0.0f};
 	//call function Kalmanfilter_C
-	Kalmanfilter_C(test_vector, output_array_C, &currentState, length);
+	Kalmanfilter_C(testVector, outputArrayC, &currentState, length);
 	
 	//initiate empty output array of size length
-	float output_array_asm[length];
+	float outputArrayASM[length];
 	
 	//reinitialize the struct at p=r=q 0.1 and x=k=0 
 	currentState.p = 0.1f ; 
@@ -68,28 +173,48 @@ int main()
 	currentState.x = 0.0f ; 
 	
 	//call subroutine Kalmanfilter_asm
-	Kalmanfilter_asm(test_vector, output_array_asm, &currentState, length );
-	//Check for correctness with a tolerance of 0.000001
-	isValid(output_array_C,output_array_asm,length, 0.000001, "c vs asm");
+	Kalmanfilter_asm(testVector, outputArrayASM, &currentState, length );
+	//Check for correctness with a error tolerance of 0.000001
+	float errorTolerance = 0.000001f; 
+	is_valid(outputArrayC, outputArrayASM, length, errorTolerance, "c vs asm");
 	
-	
-	//float state_CMSIS[4];
+	float differenceC[length];
+	float differenceCMSIS[length];
 	
 	//Difference
-	//arm_sub_f32 (float32_t *pSrcA, float32_t *pSrcB, float32_t *pDst, uint32_t blockSize);
+	arm_sub_f32 (testVector, outputArrayC, differenceCMSIS, length);
+	c_sub(testVector, outputArrayC, differenceC, length);
+	
+	is_valid(differenceC, differenceCMSIS, length, errorTolerance, "Difference");
 	
 	//Mean
-	//arm_mean_f32 (float32_t *pSrc, uint32_t blockSize, float32_t *pResult);
+	float meanCMSIS;
+	float meanC; 
+	arm_mean_f32 (differenceCMSIS, length , &meanCMSIS);
+	c_mean(differenceC,length, &meanC); 
+	is_valid(&meanC, &meanCMSIS, 1, errorTolerance, "mean"); 
 	
 	//STD
-	//arm_std_f32 (float32_t *pSrc, uint32_t blockSize, float32_t *pResult);
+	float stdC;
+	float stdCMSIS;
+	arm_std_f32 (differenceCMSIS, length, &stdCMSIS);
+	c_std(differenceC, length, &stdC);
+	is_valid(&stdC, &stdCMSIS, 1, errorTolerance, "STD");
 	
-	//correlation;
-	//arm_correlate_f32 (float32_t *pSrcA, uint32_t srcALen, float32_t *pSrcB, uint32_t srcBLen, float32_t *pDst);
+	//correlation
+	float corC[2*length-1];
+	float corCMSIS[2*length-1];
+	arm_correlate_f32 (testVector, length, outputArrayC, length, corCMSIS);
+	//c_correlate(testVector, outputArrayC, corC, 2*length-1);
+	//is_valid(corC, corCMSIS, 2*length-1, errorTolerance, "correlation"); 
 	
 	//convolution
-	//arm_conv_f32 (float32_t *pSrcA, uint32_t srcALen, float32_t *pSrcB, uint32_t srcBLen, float32_t *pDst);
-	
+	float convC[2*length-1];
+	float convCMSIS[2*length-1];
+	arm_conv_f32 (testVector, length, outputArrayC, length, convCMSIS);
+	c_conv(testVector, outputArrayC, convC, 2*length-1);
+	is_valid(convC, convCMSIS, 2*length-1, errorTolerance, "convolution"); 
+
 	return 0;
 }
 
