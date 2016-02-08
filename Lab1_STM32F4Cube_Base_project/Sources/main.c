fdf15847e81ab1c6b3040ef9bb5b6185d9d2ecf2
RichardCheung1 <li.cheung@mail.mcgill.ca>
2016-02-08 18:44:05 -0500

    added Taha comments

--- Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -1,3 +1,10 @@
+/**
+ * @file main.c
+ * @author Group21 - Taha Saifuddin, Richard Cheung
+ * @date 8 Feb 2016
+ * @brief C testbench to implement basic Kalman Filter and other statistical functions
+ */
+
 #include <stdio.h>
 #include "arm_math.h"
 #include "demo.h"
@@ -6,20 +13,28 @@ typedef struct stateInfo{
 	float q, r, x, p, k;
 }kalman_state;
 
-//subroutine Kalmanfilter_asm
+/*FUNCTION DECLARATIONS*/
+
+//Assembly subroutine
 extern int Kalmanfilter_asm (float* InputArray, float* OutputArray, kalman_state* kstate, int Length);
-//Kalmanfilter_C
+
+//C subroutine
 int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, int Length);
-//functions
+
+//Statistical functions
 void c_sub(float* vector_one, float* vector_two, float* output_vector, int vector_length);
 void c_mean(float* input_vector, int vector_length, float* vector_mean);
 void c_std(float* input_vector, int vector_length, float* vector_std);
 void c_correlate(float* vector_one, float* vector_two, float* vector_correlation, int vector_length);
 void c_conv(float* vector_one, float* vector_two, float* vector_convolution, int vector_length);
+
+//Functions to check for correctness
 void is_valid(float *ScrA, float *ScrB, int vector_length, float tol, char *errorCode);
 void is_valid_relative(float *ScrA, float *ScrB, int vector_length, float errTol, float errRelativeTol, char *errorCode);
 
-//C implementation difference
+/*FUNCTION IMPLEMENTATIONS*/
+
+//The following method takes in two vectors and their length, and calculates their corresponding difference vector 
 void c_sub(float* vector_one, float* vector_two, float* output_vector, int vector_length)
 {
 	int i;
@@ -28,7 +43,8 @@ void c_sub(float* vector_one, float* vector_two, float* output_vector, int vecto
 		output_vector[i] = vector_one[i] - vector_two[i];
 	}
 }
-//C implementation mean
+
+//The following method takes an input vector and its length, and then finds its mean
 void c_mean(float* input_vector, int vector_length, float* vector_mean)
 {
 	int i;
@@ -41,23 +57,21 @@ void c_mean(float* input_vector, int vector_length, float* vector_mean)
 	*vector_mean = sum / vector_length;
 }
 
-//C implementation standard deviation
-
+//The following method takes an input vector and its length, and calculates its standard deviation
 void c_std(float* input_vector, int vector_length, float* vector_std)
 {	
 	float sumOfSquares = 0;
 	float sum = 0;
 	int i = 0;
-	while (i < length) {
+	while (i < vector_length) {
 		sumOfSquares += input_vector[i] * input_vector[i];
 		sum += input_vector[i];
 		i++;
 	}
-	arm_sqrt_f32(((sumOfSquares - (sum * sum) / length) / (length - 1)), vector_std);
+	arm_sqrt_f32(((sumOfSquares - (sum * sum) / vector_length) / (vector_length - 1)), vector_std);
 }
 
-//C implementation standard correlation
-
+//The following method takes two vectors as input and their length, and then calculates their correlation
 void c_correlate(float* vector_one, float* vector_two, float* vector_correlation, int vector_length)
 {
 	int correlation_length = vector_length + vector_length - 1;
@@ -75,8 +89,8 @@ void c_correlate(float* vector_one, float* vector_two, float* vector_correlation
 		}
 	}
 }
-//C implementation convolution
 
+//The following method takes two vectors as input and their length, and then calculates their convolution
 void c_conv(float* vector_one, float* vector_two, float* vector_convolution, int vector_length)
 {	
 	int convolution_length = vector_length + vector_length - 1;
@@ -95,7 +109,7 @@ void c_conv(float* vector_one, float* vector_two, float* vector_convolution, int
 	}
 }
 
-//C implementation Kalmanfilter
+//The following method takes an array of measurements and filters the values using the Kalman Filter
 int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, int Length)
 {	
 	int i;
@@ -103,17 +117,22 @@ int Kalmanfilter_C(float* InputArray, float* OutputArray, kalman_state* kstate, 
 		kstate->p = kstate->p + kstate->q;
 		kstate->k = kstate->p / (kstate->p + kstate->r);
 		kstate->x = kstate->x + kstate->k * (InputArray[i]-kstate->x);
+
+		//The following conditional checks if kstate->x is not a number
 		if (kstate->x != kstate->x) {
 			printf("An error has occured -NaN value was found"); 
 			return 1;
 		}
+
 		kstate->p = (1-kstate->k) * kstate->p;
+
+		//Add the filtered value to the output array
 		OutputArray[i] = kstate->x;
 	}
 	return 0; 
 }
 
-//checks if Source A and Source B have the same value
+//The following method takes two float arrays as inputs and compares them to each other to see if they match within a given absolute error
 void is_valid(float *ScrA, float *ScrB, int vector_length, float tol, char *errorCode)
 {
 	int failed = 1;
@@ -133,7 +152,7 @@ void is_valid(float *ScrA, float *ScrB, int vector_length, float tol, char *erro
 	}
 }
 
-//same as is_valid but with relative error
+//The following method takes two float arrays as inputs and compares them to each other to see if they match within a given relative error
 void is_valid_relative(float *ScrA, float *ScrB, int vector_length, float errTol, float errRelativeTol, char *errorCode)
 {
 	int failed = 1;
@@ -188,11 +207,14 @@ int main()
 
 	//Check for correctness with a error tolerance of 0.000001
 	float errorTolerance = 0.000001f;
-	float errorPercentage = 0.01; 
+	float errorPercentage = 0.01;
+
 	//is_valid(outputArrayC, outputArrayASM, length, errorTolerance, "c vs asm");
 	//is_valid_relative(outputArrayC, outputArrayASM, length, errorTolerance, errorPercentage,"c vs asm");
+
 	int p;
 
+	//print KalmanFilter output
 	for ( p = 0; p < length;  p++ )
 	{
 		printf("OutputASM: %f & OutputC %f\n", outputArrayASM[p], outputArrayC[p]); 
@@ -263,8 +285,5 @@ int main()
 		printf("CorrelateC: %f & CorrelatCMSIS: %f \n", corC[p], corCMSIS[p]); 
 	}
 
-
-
 	return 0;
 }
-
